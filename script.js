43cb5114826809af07963cecac2f810f538816d1
nicholas cheah <115200751+cheahfulnic@users.noreply.github.com>
2024-09-26 00:50:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
+const listInfo = document.querySelector('.list-info');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+let index = 0;
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
@@ -13,4 +18,14 @@ window.addEventListener('scroll', () => {
     hill5.style.left = value * 1.5 + 'px';
     hill4.style.left = value * -1.5 + 'px';
     hill1.style.top = value * 1 + 'px';
+})
+
+nextBtn.addEventListener('click', () => {
+    index = (index < 3) ? index + 1 : 3;
+    listInfo.style.transform = `translateY(${index}%)`;
+})
+
+prevBtn.addEventListener('click', () => {
+    index = (index > 3) ? index - 1 : 0;
+    listInfo.style.transform = `translateY(${index}%)`;
 })
